4994268dc3fc9144f9f933c2dfc00e2e8e8b9ee5
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-05-14 16:33:02 +0700

    add dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,43 @@
+import '../models/course.dart';
+
+final List<Course> courses = [
+  Course(
+    id: 'c1',
+    title: 'Introduction to Python Programming',
+    authorName: 'John Doe',
+    authorAvatarUrl: '<https://via.placeholder.com/150>',
+    authorJob: 'Senior Software Engineer',
+    imageUrl: '<https://via.placeholder.com/350x200>',
+    description: 'Learn the basics of Python programming language.',
+    level: 'Beginner',
+    duration: '2 weeks',
+    price: 'Free',
+    status: null,
+  ),
+  Course(
+    id: 'c2',
+    title: 'Django Web Development',
+    authorName: 'Jane Smith',
+    authorAvatarUrl: '<https://via.placeholder.com/150>',
+    authorJob: 'Software Engineer',
+    imageUrl: '<https://via.placeholder.com/350x200>',
+    description: 'Learn how to build web applications using Django.',
+    level: 'Intermediate',
+    duration: '4 weeks',
+    price: 'Paid',
+    status: null,
+  ),
+  Course(
+    id: 'c3',
+    title: 'Machine Learning with Python',
+    authorName: 'Bob Johnson',
+    authorAvatarUrl: '<https://via.placeholder.com/150>',
+    authorJob: 'Data Scientist',
+    imageUrl: '<https://via.placeholder.com/350x200>',
+    description: 'Learn how to build machine learning models using Python.',
+    level: 'Advanced',
+    duration: '6 weeks',
+    price: 'Paid',
+    status: 'New',
+  ),
+];
